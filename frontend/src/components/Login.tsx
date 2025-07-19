ed93e2f5627c672a04a8953c50c47f3c8c7e173a
Jordi-Maldonado <Jordi.A.Maldo@gmail.com>
2025-07-19 17:54:19 -0400

    Update Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -39,7 +39,7 @@ function Login()
             alert(error.toString());
             return;
         }    
-      };
+      }
 
     function handleSetLoginName( e: any ) : void
     {
@@ -74,7 +74,3 @@ function Login()
 };
 
 export default Login;
-};
-
-export default Login;
-
