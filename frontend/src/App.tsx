f24b67e05e9eb32cfcadc96beee189b00e1ec951
Jordi-Maldonado <Jordi.A.Maldo@gmail.com>
2025-07-19 17:08:26 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,19 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import ShowPage from './pages/ShowPage';
+
 function App() {
  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/shows" element={<ShowPage />} />
       </Routes>
     </BrowserRouter>
  );
 }
+
 export default App;
