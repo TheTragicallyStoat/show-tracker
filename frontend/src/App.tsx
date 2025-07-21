445159834c2bd863dc9670f9092f185e91b99c04
Jordi-Maldonado <Jordi.A.Maldo@gmail.com>
2025-07-21 17:12:30 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ShowPage from './pages/ShowPage';
+import VerifyEmailPage from './pages/VerifyEmailPage';
+import VerifyCodePage from './pages/VerifyCodePage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
 function App() {
  return (
@@ -12,6 +16,10 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/shows" element={<ShowPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+        <Route path="/verify-code" element={<VerifyCodePage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
       </Routes>
     </BrowserRouter>
  );
